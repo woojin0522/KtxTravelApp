ad2a70e9a5bb7f37333ab3f5f7e5dd8878f38043
woojin0522 <131342064+woojin0522@users.noreply.github.com>
2023-11-18 00:13:54 +0900

    "commit"

--- app/src/main/java/com/example/ktxtravelapplication/MapActivity.kt
+++ app/src/main/java/com/example/ktxtravelapplication/MapActivity.kt
@@ -2,11 +2,15 @@ package com.example.ktxtravelapplication
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.annotation.UiThread
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ktxtravelapplication.databinding.ActivityMapBinding
+import com.google.android.material.navigation.NavigationView
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraAnimation
 import com.naver.maps.map.CameraPosition
@@ -29,10 +33,12 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         setSupportActionBar(binding.mapToolbar)
         supportActionBar!!.setTitle("")
 
+        // 뒤로가기 버튼
         binding.mapBackBtn.setOnClickListener{
             finish()
         }
 
+        // 드로어 레이아웃 열고 닫기 버튼
         binding.mapDrawerBtn.setOnClickListener {
             if(binding.mapNavView.isActivated == false){
                 binding.mapDrawer.openDrawer(binding.mapNavView, true)
@@ -44,15 +50,34 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
+        // 뒤로가기 버튼
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // 드로어 레이아웃이 열려있을 떄 뒤로가기 버튼을 누를경우 드로어 레이아웃을 닫는다.
+                if(binding.mapNavView.isActivated == true) {
+                    binding.mapDrawer.closeDrawer(binding.mapNavView, true)
+                    binding.mapNavView.isActivated = false
+                }
+                // 드로어 레이아웃이 닫혀있다면 메인화면으로 돌아간다.
+                else{
+                    finish()
+                }
+            }
+        }
+        onBackPressedDispatcher.addCallback(this, callback)
+
+        // mapFragment 설정
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
             ?: MapFragment.newInstance().also {
                 fm.beginTransaction().add(R.id.map_fragment, it).commit()
             }
 
+        // 네이버 객체 얻기
         mapFragment.getMapAsync(this)
     }
 
+    // 네이버 지도 객체 준비
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
         val uiSettings = naverMap.uiSettings
