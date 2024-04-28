ca1d5efa0127ef873e89a6c8941ed314cc3ebb46
woojin0522 <131342064+woojin0522@users.noreply.github.com>
2024-04-28 17:31:55 +0900

    commit

---
+++ app/src/main/java/com/example/ktxtravelapplication/mapActivity/RecommendInfoFragment.kt
@@ -0,0 +1,33 @@
+package com.example.ktxtravelapplication.mapActivity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.ktxtravelapplication.R
+import com.example.ktxtravelapplication.databinding.FragmentRecommendInfoBinding
+
+class RecommendInfoFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    companion object {
+        fun newInstance() : RecommendInfoFragment{
+            val fragment = RecommendInfoFragment()
+
+            return fragment
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentRecommendInfoBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
